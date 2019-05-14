b1c8ed1ff2275e8869f5b1946f33607ac5a24972
github username <github email address>
2019-05-14 11:14:57 +0100

    added welcome and user input command

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
+
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to the Star Wars character bio wiki!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+puts "please enter a character name"
+input = gets.chomp.downcase
+# use gets to capture the user's input. This method should return that input, downcased.
 end
