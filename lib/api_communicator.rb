2d4c8a5a2bea93fa5141587d57d9ae989198555c
github username <github email address>
2019-05-14 13:24:09 +0100

    Corrected mistakes and resubmitted ruby file

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,50 +2,50 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+def get_character_movies_from_api(character_name) # WORKS
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  film_array = []
-  collected_films = []
-  response_hash["results"].each do |characters|
-   if character_name = characters["name"]
-      film_array << characters["films"]
-   else 
-      nil
+   response_string = RestClient.get('http://www.swapi.co/api/people/')
+   response_hash = JSON.parse(response_string)
+   film_array = []
+   film_urls = []
+   response_hash["results"].each do |characters|
+      if character_name == characters["name"].downcase
+         film_urls = characters["films"]
+      else 
+         nil
+      end
    end
-end
-   film_array[0].each do |film_url|
-      film_array_response_string = RestClient.get(film_url)
-      collected_films << film_array_response_hash = JSON.parse(film_array_response_string)
-   end
-   collected_films
-# iterate over the response hash to find the collection of `films` for the given
-#   `character` – DONE
-# collect those film API urls, make a web request to each URL to get the info
-#  for that film – DONE
-# return value of this method should be collection of info about each film.
-      #  i.e. an array of hashes in which each hash reps a given film – DONE
-# this collection will be the argument given to `print_movies`
-#  and that method will do some nice presentation stuff like puts out a list
-#  of movies by title. Have a play around with the puts with other info about a given film.
+   film_urls
 end
 
 def print_movies(films) 
-   # some iteration magic and puts out the movies in a nice list
-   # response_hash.each do |details|
-   #    response_hash[details]
-   puts films
+   titles = []
+   i=0
+   while i < films.length 
+      film_array_response_string = RestClient.get(films[i]) #PROBLEM IS HERE
+      film_array_response_hash = JSON.parse(film_array_response_string)
+      titles << film_array_response_hash["title"]
+      i+=1
+   end
+  puts titles
 end
 
-# binding.pry 
 def show_character_movies(character)
    films = get_character_movies_from_api(character)
-     print_movies(films)
-   # binding.pry
+   print_movies(films)
 end
 
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+# iterate over the response hash to find the collection of `films` for the given
+#   `character` – DONE
+# collect those film API urls, make a web request to each URL to get the info
+#  for that film – DONE
+# return value of this method should be collection of info about each film.
+#  i.e. an array of hashes in which each hash reps a given film – DONE
+# this collection will be the argument given to `print_movies`
+#  and that method will do some nice presentation stuff like puts out a list
+#  of movies by title. Have a play around with the puts with other info about a given film.
